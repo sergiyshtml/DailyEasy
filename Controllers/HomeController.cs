21579bdcde2531182f6eb79dbd4b78d96a5d1e78
sergiyshtml <s.vorobey27@gmail.com>
2025-01-30 19:31:09 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using DailyEasy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyEasy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        public IActionResult Delivery()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
